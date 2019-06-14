7cd05da0bc672d85a2b481341fe1daa9b7a9dd5e
Krittanan Pingclasai <kp@kpping.me>
2019-06-14 17:22:17 +0700

    re-built

--- lib/get_all_columns/index.d.ts
+++ lib/get_all_columns/index.d.ts
@@ -10,8 +10,10 @@ export interface IGetAllColumnsResult {
     NUMERIC_PRECISION: number | null;
     NUMERIC_SCALE: number | null;
     IsIdentity: 0 | 1;
-    column_type: string;
-    column_key: string;
+    column_type?: string;
+    COLUMN_TYPE?: string;
+    column_key?: string;
+    COLUMN_KEY?: string;
 }
 export declare function getAllColumns(tableName: string, connection: Connection): import("rxjs").Observable<[IGetAllColumnsResult[], import("mysql").FieldInfo[] | undefined]>;
 //# sourceMappingURL=index.d.ts.map
--- lib/get_ts_type/index.d.ts
+++ lib/get_ts_type/index.d.ts
@@ -1,4 +1,4 @@
-export declare function getTsType(dataType: string, columnType: string, isNullable: string): string;
+export declare function getEnumOrSetType(dataType: string, columnType: string): string;
 export declare function getMainType(dataType: string, columnType: string): string;
-export declare function getEnumType(columnType: string): string;
+export declare function getTsType(dataType: string, columnType: string, isNullable: string): string;
 //# sourceMappingURL=index.d.ts.map
